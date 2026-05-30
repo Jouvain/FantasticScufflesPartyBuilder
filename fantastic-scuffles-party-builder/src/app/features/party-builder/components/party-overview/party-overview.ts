fb28d263f6b415f73938a67c29582a17469aa1d1
Cyril FERRER <cyril.ferrer31@gmail.com>
2026-05-30 16:12:16 +0200

    basic layout WIP 2

--- fantastic-scuffles-party-builder/src/app/features/party-builder/components/party-overview/party-overview.ts
+++ fantastic-scuffles-party-builder/src/app/features/party-builder/components/party-overview/party-overview.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { PartyStats } from "../party-stats/party-stats";
-import { PartyControls } from "../party-controls/party-controls";
+import { BtnAction } from "../btn-action/btn-action";
+
 
 @Component({
   selector: 'app-party-overview',
-  imports: [PartyStats, PartyControls],
+  imports: [BtnAction],
   templateUrl: './party-overview.html',
   styleUrl: './party-overview.less',
 })
--- fantastic-scuffles-party-builder/src/app/features/party-builder/components/party-stats/party-stats.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-party-stats',
-  imports: [],
-  templateUrl: './party-stats.html',
-  styleUrl: './party-stats.less',
-})
-export class PartyStats {}
