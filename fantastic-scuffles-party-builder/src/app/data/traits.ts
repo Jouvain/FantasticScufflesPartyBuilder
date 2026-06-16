953bacbdb59a1bdf020043d87bfaf52247876e39
Cyril FERRER <cyril.ferrer31@gmail.com>
2026-06-16 10:42:56 +0200

    feat (data): add traits

--- fantastic-scuffles-party-builder/src/app/data/traits.ts
+++ fantastic-scuffles-party-builder/src/app/data/traits.ts
@@ -647,6 +647,291 @@ export const TRAITS: Trait[] = [
         ]
     },
     {
+        id: 45,
+        name: "Scion",
+        special: "Roll 1D10 at the start on the game. The scion is hated/ignored/loved by NPC : 1-2 = hated - opponents may choose +/-1 on NPC Activation roll ; 3-6 = nothing ; 7+ = loved - can choose +/-1 on NPV Activation rolls",
+        cost: 1,
+        characterOnly: true,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 46,
+        name: "Shieldwall",
+        special: "If has a shield and in contact with a shielded friend also with 'Shieldwall', +1 DvA/DvS on front",
+        cost: 1,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 47,
+        name: "Skilled Fighter",
+        special: "",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "attH1",
+                modifier: 1
+            },
+            {
+                kind: "direct-stat",
+                stat: "attH2",
+                modifier: 1
+            }
+        ]
+    },
+    {
+        id: 48,
+        name: "Skilled Marksman",
+        special: "",
+        cost: 3,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "sht",
+                modifier: 1
+            }
+        ]
+    },
+    {
+        id: 49,
+        name: "Slow",
+        special: "",
+        cost: -1,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "mov",
+                modifier: -1
+            }
+        ]
+    },
+    {
+        id: 50,
+        name: "Snapshot",
+        special: "Can shoot at charging/contacting ennemy with -1 Sht IF non already in contact",
+        cost: 3,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 51,
+        name: "Social",
+        special: "+1 to Resolve rolls for each friend within 2''",
+        cost: 1,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "res",
+                modifier: 1
+            }
+        ]
+    },
+    {
+        id: 52,
+        name: "Spikey",
+        special: "May carry out opportunity attack for each hit suffered",
+        cost: 4,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 53,
+        name: "Steady Aim",
+        special: "+1 Sht IF not moving the same turn",
+        cost: 1,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 54,
+        name: "Stealthy",
+        special: "+2 DvS IF in base contact with any terrain feature ; +2 DvA IF also in the dark",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 55,
+        name: "Stimulants",
+        special: "At start, roll 1D10 for effect : 1 = -1 to rolls ; 2 = -1 Att ; 3 = -1 Mov ; 4-5 = no effect ; 6 = ignore first wound ; 7 = +1 Mov = 8 = + Att ; 9 = +1 to Resolve ; 10 = -1rtW to ennemies",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 56,
+        name: "Strong",
+        special: "Can carry same-sized without being encumbered, +1 rtW for Attacks",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 57,
+        name: "Swarm",
+        special: "On death, each ennemy in contact suffers 1 Wound on a 1D10 = 8+ ; Pass all Resolve ; Do not cause Resolve on death ; - 1 rtW for Attacks",
+        cost: 1,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "wnd",
+                modifier: -1
+            },
+            {
+                kind: "direct-stat",
+                stat: "dva",
+                modifier: 2
+            },
+            {
+                kind: "direct-stat",
+                stat: "dvs",
+                modifier: 3
+            }
+        ]
+    },
+    {
+        id: 58,
+        name: "Tail swipe",
+        special: "May carry out opportunity attacks on rear",
+        cost: 3,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 59,
+        name: "Taunt",
+        special: "Use 1 Interact Action : an ennemy within 8'' must Resolve OR charge full move the taunter (once/turn by ennemy)",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 60,
+        name: "Terrifying",
+        special: "Ennemies must Resolve to charge (failure = Action wasted) ; Ennemies suffers -2 Att against self",
+        cost: 8,
+        characterOnly: true,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 61,
+        name: "Tough",
+        special: "",
+        cost: 2,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "wnd",
+                modifier: 1
+            }
+        ]
+    },
+    {
+        id: 62,
+        name: "Tracker",
+        special: "+2 Sht againts 'ephemeral' ennemies ; May attack/shot 'invisible' annemies with -1 ; The party can reroll their deployment dice ",
+        cost: 3,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "wnd",
+                modifier: 1
+            }
+        ]
+    },
+    {
+        id: 63,
+        name: "Trapper",
+        special: "Can set up traps within 12'' of self during deployment (3 if character, 1 if minion)",
+        cost: 5,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 64,
+        name: "Tumbler",
+        special: "Can pass through a single ennemy without being stopped or attacked",
+        cost: 4,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
+        id: 65,
+        name: "Unarmed Fighter",
+        special: "+1 rtW for Attacks",
+        cost: 7,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+            {
+                kind: "direct-stat",
+                stat: "dva",
+                modifier: 1
+            },
+            {
+                kind: "direct-stat",
+                stat: "attH1",
+                modifier: 2
+            },
+            {
+                kind: "direct-stat",
+                stat: "attH2",
+                modifier: 2
+            }
+        ]
+    },
+    {
+        id: 66,
+        name: "Wayfinder",
+        special: "Ignore all Mov/Att modifiers for terrestrial terrain",
+        cost: 3,
+        characterOnly: false,
+        type: 'trait',
+        profileAdjustment: [
+        ]
+    },
+    {
         id: 100,
         name: "Bless",
         special: "the target gains +5 to their next die roll",
