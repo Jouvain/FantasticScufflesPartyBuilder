528c9051d799831c1f8c6b2919b04e95e31f79d7
Cyril FERRER <cyril.ferrer31@gmail.com>
2026-06-18 12:07:33 +0200

    feat(roster): click disabled if drawer open

--- fantastic-scuffles-party-builder/src/app/features/party-builder/components/roster/roster.ts
+++ fantastic-scuffles-party-builder/src/app/features/party-builder/components/roster/roster.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, inject, output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject, output } from '@angular/core';
 import { UnitCard } from "../unit-card/unit-card";
 import { PartyStore } from '../../services/party-store';
+import { DrawerStore } from '../../services/drawer-store';
 
 @Component({
   selector: 'app-roster',
@@ -11,4 +12,6 @@ import { PartyStore } from '../../services/party-store';
 })
 export class Roster {
   public readonly partyStore: PartyStore = inject(PartyStore);
+  public readonly drawerStore: DrawerStore = inject(DrawerStore);
+  cursorIsEnabled = computed(() => this.drawerStore.isOpen());
 }
