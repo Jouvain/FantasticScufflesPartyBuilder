0a93ef24fe2d44b93635b5243dad98ad239bddeb
Cyril FERRER <cyril.ferrer31@gmail.com>
2026-06-19 15:57:23 +0200

    feat (validations): completed validators in form

--- fantastic-scuffles-party-builder/src/app/features/party-builder/rules/profile-restriction-rules.ts
+++ fantastic-scuffles-party-builder/src/app/features/party-builder/rules/profile-restriction-rules.ts
@@ -1,17 +1,17 @@
-// minio = 0 spell
 
-import { ValidatorFn } from "@angular/forms";
+import { FormControl, FormGroup, ValidatorFn } from "@angular/forms";
 import { Archetype } from "../../../types/archetype"
 import { Trait } from "../../../types/trait-type";
 import { ArmorDefinition } from "../../../models/armor-definition";
-
-// warrior-rogue = 1 spell
-
-// agile = no armour/shield
-
-// double grip OU 2* single
+import { EquippedMeleeWeapon } from "../../../models/equipped-melee-weapon";
+import { Size } from "../../../types/size";
+import { MiscDefinition, MissileEnhancementDefinition, StandaloneMiscDefinition } from "../../../models/misc-definition";
+import { MissileWeaponDefinition } from "../../../models/missile-weapon-definition";
 
 // shield = uniquement renowned shield
+// no mount for flying model
+// only small minions as Swarms
+
 
 type ProfileRestrictionFormValue = {
     archetype: Archetype;
@@ -20,6 +20,11 @@ type ProfileRestrictionFormValue = {
     trait3: Trait | null;
     trait4: Trait | null;
     armour: ArmorDefinition;
+    hand1: EquippedMeleeWeapon;
+    hand2: EquippedMeleeWeapon;
+    size: Size;
+    misc1: MiscDefinition | null;
+    misc2: MiscDefinition | null;
 };
 
 export interface ProfileRestrictionViolation {
@@ -40,6 +45,12 @@ export const profileRestrictionValidator: ValidatorFn = (control) => {
     const hasSpell = traits.some((trait) => trait.type === "spell");
     const spellsNb = traits.filter((trait) => trait.type === "spell").length;
     const isAgile = traits.some((trait) => trait.name === "Agile");
+    const hasShield = value.hand1.weapon.name.includes("hield") || value.hand2.weapon.name.includes("hield");
+    const hasDoubleGrip = value.hand1.weapon.grip === "double" || value.hand2.weapon.grip === "double"; 
+    const hasTwoWeapons = value.hand1.weapon.name != "No weapon" && value.hand2.weapon.name != "No weapon";
+    const hasCharacterOnlyTrait = traits.some((trait) => trait.characterOnly);
+    const characterOnlyTraits = traits.filter((trait) => trait.characterOnly);
+    const isMounted = value.misc1?.name.includes("Mount") || value.misc2?.name.includes("Mount");
     const violations: ProfileRestrictionViolation[] = [];
 
     if(value.archetype === "minion" && hasSpell) {
@@ -54,11 +65,41 @@ export const profileRestrictionValidator: ValidatorFn = (control) => {
             message: "rogue and warriors cannot have more than 1 spell"
         });
     }
-    if(isAgile && value.armour?.name != "No armour") {
+    if(isAgile && (value.armour?.name != "No armour" || hasShield)) {
         violations.push({
             code: "agile-armored",
             message: "an 'Agile' model cannot wear armour"
         });        
+    }
+    if(hasDoubleGrip && hasTwoWeapons) {
+        violations.push({
+            code: "too-much-weapons",
+            message: "the model has too much weapons"
+        });         
+    }
+    if(value.archetype === "minion" && hasCharacterOnlyTrait) {
+        violations.push({
+            code: "character-only",
+            message: `a minion cannot have ${characterOnlyTraits[0].name}`
+        });            
+    }
+    if((value.hand1.weapon.name.includes("hield") && (value.hand1.enhancement && value.hand1.enhancement?.name != "Renowned shield")) || (value.hand2.weapon.name.includes("hield") && (value.hand2.enhancement && value.hand2.enhancement?.name != "Renowned shield"))) {
+        violations.push({
+            code: "shield-too-custom",
+            message: `a shield can only be Renowned`
+        }); 
+    }
+    if(traits.some((trait) => trait.name === "Swarm") && (value.archetype != "minion" || value.size != "small")) {
+        violations.push({
+            code: "only-small-swarms",
+            message: `a Swarm must be a small minion`
+        }); 
+    }
+    if(traits.some((trait)=> trait.name === "Flying") && isMounted) {
+        violations.push({
+            code: "flyer-is-mounted",
+            message: `a Flyer cannot have a mount`
+        });         
     }
 
     return violations.length > 0 ? {profileRestrictionViolations: violations} : null;
